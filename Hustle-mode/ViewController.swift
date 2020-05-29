1b9795262e8b3d237eb70c35e10b1e7acbbfb860
OkonovUrmat <okonov.okonov@gmail.com>
2020-05-29 21:45:51 +0600

    ViewController: Refactor code

--- Hustle-mode/ViewController.swift
+++ Hustle-mode/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var darkBlueBG: UIImageView!
     @IBOutlet weak var powerBtn: UIButton!
     @IBOutlet weak var cloudHolder: UIView!
@@ -25,15 +25,14 @@ class ViewController: UIViewController {
         let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")! //Указываем файл который нам нужен и где он находится
         let url = URL(fileURLWithPath: path)//Нахождение файла
         do{
-            player = try AVAudioPlayer(contentsOf: url)//Присваивание файла hustle-on.wav к переменной player
-            player.prepareToPlay()//Подготавливаенм к воспроизведению
+            player = try AVAudioPlayer(contentsOf: url) //Присваивание файла hustle-on.wav к переменной player
+            player.prepareToPlay() //Подготавливаенм к воспроизведению
         }
         catch let error as NSError {
             print (error.description)
         }
     }
-
-
+    
     @IBAction func powerBtnPressed(_ sender: Any) {
         cloudHolder.isHidden = false
         darkBlueBG.isHidden = true
@@ -41,11 +40,11 @@ class ViewController: UIViewController {
         
         player.play()
         
-        UIView.animate(withDuration: 2.3 /*Длительность анимации*/ , animations: {
-            self.rocket.frame = CGRect (x: 20, y: 140, width: 375, height: 402) //Движение ракеты
-        }) { (finished) in //При прохождение анимации
-            self.hustleLbl.isHidden = false//Показываем надпись hustle mode
-            self.onLbl.isHidden = false//Показыввем надпись On
+        UIView.animate(withDuration: 2.3 /* Длительность анимации */ , animations: {
+            self.rocket.frame = CGRect (x: 20, y: 140, width: 375, height: 402) // Движение ракеты
+        }) { (finished) in // При прохождение анимации
+            self.hustleLbl.isHidden = false// Показываем надпись hustle mode
+            self.onLbl.isHidden = false// Показыввем надпись On
         }
     }
 }
